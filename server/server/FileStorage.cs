1606c0ac2cc11cb72853148044e6d6ddce94a404
wingwii <wingwii1987@gmail.com>
2022-01-09 19:03:47 +0700

    Add storage server

---
+++ server/server/FileStorage.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.MemoryMappedFiles;
+using System.Text;
+using System.Threading;
+
+namespace server
+{
+    class FileStorage
+    {
+        private List<object[]> _lstGlobalRef = new List<object[]>();
+
+        public FileStorage()
+        {
+            //
+        }
+
+        public string NamePrefix { get; set; } = string.Empty;
+
+        public int FileCount { get; set; } = 0;
+
+        public long MaxFileSize { get; set; } = 0;
+
+        public void Start()
+        {
+            long capacity = 4 + (2 * (12 + this.MaxFileSize));
+            var n = this.FileCount;
+            for (int i = 0; i < n; ++i)
+            {
+                var name = this.NamePrefix + i.ToString();
+                var mutexName = name + ".lck";
+
+                var mutex = new Mutex(false, mutexName);
+                var mmf = MemoryMappedFile.CreateNew(name, capacity);
+                var view = mmf.CreateViewAccessor(0, capacity);
+
+                _lstGlobalRef.Add(new object[] { mutex, mmf, view });
+            }
+        }
+
+        //
+    }
+}
---
+++ server/server/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace server
+{
+    class Program
+    {
+        private static string _FFmpeg = string.Empty;
+        private static int _MaxInstance = 4;
+        private static FileStorage _Storage = null;
+
+
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("server <configuration file>");
+                return;
+            }
+
+            var cfg = new Dictionary<string, string>(StringComparer.OrdinalIgnoreCase);
+            LoadConfigurationFile(args[0], cfg);
+
+            ApplyConfiguration(cfg);
+
+            _Storage.Start();
+
+            Console.ReadKey();
+        }
+
+        private static void LoadConfigurationFile(string path, Dictionary<string, string> result)
+        {
+            var rows = File.ReadAllLines(path);
+            foreach (var row in rows)
+            {
+                if (row.StartsWith("#", StringComparison.Ordinal))
+                {
+                    continue;
+                }
+
+                var pos = row.IndexOf('=');
+                if (pos < 0)
+                {
+                    continue;
+                }
+
+                var name = row.Substring(0, pos).Trim();
+                var value = row.Substring(pos + 1).Trim();
+                result[name] = value;
+            }
+        }
+
+        private static void ApplyConfiguration(Dictionary<string, string> cfg)
+        {
+            _FFmpeg = cfg["ffmpeg"];
+
+            var namePrefix = cfg["name_prefix"];
+            _MaxInstance = int.Parse(cfg["max_instance"]);
+
+            _Storage = new FileStorage();
+            _Storage.FileCount = _MaxInstance;
+            _Storage.MaxFileSize = long.Parse(cfg["max_data_size"]);
+            _Storage.NamePrefix = namePrefix + "_s1_";
+
+            //
+        }
+
+
+        //
+    }
+}
